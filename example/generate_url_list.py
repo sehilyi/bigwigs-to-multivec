0b00058d3c716be387db242b028f122d489ec9b1
Sehi L'Yi <sehi_lyi@hms.harvard.edu>
2020-03-03 16:46:20 -0500

    Update script with bug fixing

--- example/generate_url_list.py
+++ example/generate_url_list.py
@@ -1,5 +1,4 @@
-import urllib.request, json 
-from urllib.error import HTTPError
+import urllib.request, json
 
 # Download:
 # wget -i input_list.txt
@@ -10,20 +9,27 @@ def request():
     Get the list of BigWig file urls by looking into all Cistrome DB Browser pages.
     """
 
-    default_url = "http://dc2.cistrome.org/api/main_filter_ng?allqc=false&cellinfos=all&completed=false&curated=false&factors=all&keyword=&page={}&peakqc=false&run=false&species=Homo+sapiens"
+    allqc = True
+    factors = "H3K27ac" # or "all"
+    keyword = "h3k27ac" # or ""
+    default_url = "http://dc2.cistrome.org/api/main_filter_ng?allqc={}&cellinfos=all&completed=false&curated=false&factors={}&keyword={}&page={}&peakqc=false&run=false&species=Homo+sapiens"
+    limit_num = 300
+    
     num_pages = -1
     url_list = []
 
     # Get metadata (e.g., number of pages)
-    with urllib.request.urlopen(default_url.format(1)) as url:
+    with urllib.request.urlopen(default_url.format(allqc, factors, keyword, 1)) as url:
         json_result = json.loads(url.read().decode())
         num_pages = json_result["num_pages"]
     print("Total num of pages:", num_pages)
     
     # Get lists
+    done_processing = False
     for p in range(num_pages):
         try:
-            with urllib.request.urlopen(default_url.format(p + 1)) as url:
+            with urllib.request.urlopen(default_url.format(allqc, factors, keyword, p + 1)) as url:
+                json_result = json.loads(url.read().decode())
                 datasets = json_result["datasets"]
 
                 for d in datasets:
@@ -31,12 +37,23 @@ def request():
                     new_url = "http://dbtoolkit.cistrome.org/api_bigwig?sid={}".format(cid)
                     if new_url not in url_list:
                         url_list += [new_url]
-        except HTTPError as e:
-            print("Error requesting", default_url.format(p + 1), e.read())
-        if(p % 100 is 0):
+                        if len(url_list) >= limit_num:
+                            done_processing = True
+                    if done_processing:
+                        break
+        except:
+            print("Error requesting", default_url.format(allqc, factors, keyword, p + 1))
+        
+        if(p % 100 is 1):
             print("Done processing page", p)
+
+        if done_processing:
+            break
                 
-    with open('./example/input_list.txt', 'w') as f_out:
+    with open('./sample_input/input_list.txt', 'w') as f_out:
+        # Write parameters.
+        f_out.writelines("allqc={}\tfactors={}\tkeyword={}\tlimit_num={}\n".format(allqc, factors, keyword, limit_num))
+
         for u in url_list:
             f_out.writelines(u + "\n")
 
